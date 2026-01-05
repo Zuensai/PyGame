9680d86f3ac213a113e09060f678bd2a3ea6f688
Johan Timmermans <j.g.timmermans@outlook.com>
2026-01-05 15:03:10 +0100

    Game loop toegevoegd en basic framework.

---
+++ source/bat_game.py
@@ -0,0 +1,42 @@
+import pygame
+from pygame import Color
+from pygame.locals import QUIT
+import sys
+
+ 
+pygame.init() 
+
+
+FPS = 60
+clock = pygame.time.Clock()
+
+WHITE = Color(255, 255, 255)
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+
+
+DISPLAY_SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+pygame.display.set_caption("Game")
+
+
+def main():
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+        DISPLAY_SCREEN.fill(WHITE)
+
+        pygame.display.update()
+        
+
+        clock.tick(FPS)
+        
+
+
+if __name__ == "__main__":
+    main()
